e32bc805e6fc545fcc080af07e30fa74d00d4d76
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2022-07-27 15:25:40 +0300

    generate random users

--- server/services/users.service.js
+++ server/services/users.service.js
@@ -3,12 +3,18 @@ const fetch = require('node-fetch');
 class UsersService {
   constructor() {
     this.apiUrl = 'https://randomuser.me/api';
+    this.minResults = 50;
+    this.maxResults = 200;
   }
 
   getUsers(request, response) {
-    fetch(`${this.apiUrl}?results=150`)
+    fetch(`${this.apiUrl}?results=${this.getResultsValue()}`)
       .then(res => res.json())
       .then(data => response.send(data));
+  }
+
+  getResultsValue() {
+    return Math.floor(Math.random() * (this.maxResults - this.minResults + 1) + this.minResults);
   }
 }
 
